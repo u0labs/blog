a033e68ec01c0534f8a641eee4e1caa0bd04ee2c
adrianromanko <adrianromanko@users.noreply.github.com>
2025-01-12 01:51:42 +0000

    deploy: u0labs/blog-internal@a185cdd1bc3f3dc19f411b3e91cb8f76f6e7100c

---
+++ assets/js/ef8b811a.85559e24.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[8947],{6600:s=>{s.exports=JSON.parse('{"authors":[{"name":"wodsai","url":"https://wods.ai","title":"Writer @ wodsai","page":{"permalink":"/blog/authors/wodsai"},"imageURL":"/blog/img/authors/wods_logo_2.svg","key":"wodsai","count":4}]}')}}]);
