1a83c4f8ebc5c2cbc303ddc5bb3c61b89921cd2b
adrianromanko <adrianromanko@users.noreply.github.com>
2025-03-16 14:05:39 +0000

    deploy: u0labs/blog-internal@750739eb220f239a72f7e230038721805fa4ad77

--- assets/js/3f9ae9f6.66d4e5de.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[3032],{8322:s=>{s.exports=JSON.parse('{"authors":[{"name":"wodsai","url":"https://wods.ai","title":"Writer @ wodsai","page":{"permalink":"/authors/wodsai"},"imageURL":"/img/authors/wods_logo_2.svg","key":"wodsai","count":4}]}')}}]);
---
+++ assets/js/47b584ce.2f7228a4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[4827],{9784:a=>{a.exports=JSON.parse('{"tag":{"label":"VR","permalink":"/tags/vr","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/vr","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
