0187250a7ab3064d17ab296ba4e0352ad7e70303
adrianromanko <adrianromanko@users.noreply.github.com>
2025-04-28 02:38:47 +0000

    deploy: u0labs/blog-internal@5049ab063557ec7776284acbdd0a69aed3d7df3d

---
+++ assets/js/8609a35f.9b15d4e1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[4305],{3828:a=>{a.exports=JSON.parse('{"tag":{"label":"music","permalink":"/tags/music","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/music","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
